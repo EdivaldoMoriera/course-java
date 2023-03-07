9999e4e74ec861e59e3f40dce065b17369404e22
Moreira <valdosm.moreira@gmail.com>
2023-03-07 01:41:17 -0300

    por id

--- src/main/java/com/valdosm/course/service/UserService.java
+++ src/main/java/com/valdosm/course/service/UserService.java
@@ -3,6 +3,7 @@ package com.valdosm.course.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 import com.valdosm.course.entities.User;
 import com.valdosm.course.repository.UserRepository;
@@ -14,6 +15,10 @@ public class UserService {
     public List<User> findAll(){
         return  userRepository.findAll();
 
+    }
+    public User findById(Integer id){
+        Optional <User>obj =  userRepository.findById(id);
+        return obj.get();
     }
     
 }

7355958bdd309fc74a118ea68cb2273011d2d6c2
Moreira <valdosm.moreira@gmail.com>
2023-03-09 20:30:40 -0300

    todos os porducts e por ids

---
+++ src/main/java/com/valdosm/course/service/ProductService.java
@@ -0,0 +1,24 @@
+package com.valdosm.course.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.valdosm.course.entities.Product;
+import com.valdosm.course.repository.ProductRepository;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+    public List<Product> findAll(){
+        return productRepository.findAll();
+    }
+    public Product findById(Integer id){
+        Optional<Product> obj = productRepository.findById(id);
+        return obj.get();
+    }
+    
+}
